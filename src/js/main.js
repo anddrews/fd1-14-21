3e34b12aaa81c61610591d94e1178854d9026cce
andrei.kandratsenka <anddrews@tut.by>
2021-10-06 19:36:54 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
-//here will by JavaScript code ...
+new Swiper('#slider1', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+//   new Swiper('#slider2', {
+//     loop: true,
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
