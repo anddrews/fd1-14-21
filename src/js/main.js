d9c2784c3627900b7d1a7f3d7d4d919cedff8b17
andrei.kandratsenka <anddrews@tut.by>
2021-10-13 19:52:34 +0300

    some changes

--- src/js/main.js
+++ src/js/main.js
@@ -133,19 +133,70 @@ const g = pow(10, 2);
 
 // console.log('result ' + g);
 
-const numbers = [5, 4, 3, 2, 1, [1, 'two', 3 ,3], {a: 3}, 'jksdhfksdjh'];
+// const numbers = [5, 4, 3, 2, 1, [1, 'two', 3 ,3], {a: 3}, 'jksdhfksdjh'];
 
 // console.log(numbers[5][1]);
 
-const arr = ['zero', 'one', 'two', 'tree', 'four', 'five'];
+const arr = ['zero', 'one', 'two', 'tree', 'four', 'five', , 2];
 
-for (var i = 1; i < arr.length; i+= 2) {
-    console.log(arr[i]);
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+const numbers = [4, 6, 1, -9, 45, 33];
+// 4 > 6 
+// 4 > 1 -> 4 <-> 1 [1, 6, 4, -9, 45, 33]
+// 1 > -9 -> 1 <-> -9 [-9, 6, 4, 1, 45, 33]
+// -9 > 45
+// -9 > 33
+// [-9, 6, 4, 1, 45, 33]
+// [-9, 1, 4, 6, 45, 33]
+// [-9, 1, 4, 6, 45, 33]
+// [-9, 1, 4, 6, 45, 33]
+// [-9, 1, 4, 6, 33, 45]
+// console.log(numbers);
+
+// for (var i = 0; i < numbers.length - 1; i++) {
+//     for (var j = i + 1; j < numbers.length; j++) {
+//         if (numbers[i] > numbers[j]) {
+//             var tmp = numbers[i];
+//             numbers[i] = numbers[j];
+//             numbers[j] = tmp;
+//         }
+//     }
+//     // console.log(`${i + 1}  ${numbers}`);
+// }
+
+console.log(numbers.sort(function(a, b) {
+    return b - a;
+}))
+// console.log(numbers);
+
+// ''
+// 'index * number = index * number and if index < 9 add \n'
+
+const birds = ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"];
+const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+
+function isGoos (bird) {
+    return !geese.includes(bird);
+  }
+
+
+function gooseFilter (birds) {
+    return birds.find(function(bird) {
+        return bird == 'Mallard sdfd';
+    });
+    
 }
 
+console.log(gooseFilter(birds));
 
+var arr1 = [1, 2, 3, 4];
+var arr2 = [5, 6, 7, 8];
 
-
-
+console.log(arr1.concat(arr2))
 
 
