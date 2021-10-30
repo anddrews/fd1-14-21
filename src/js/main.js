7683611f3ea4e19061b9a5f5981e5d8d10aae63c
andrei.kandratsenka <anddrews@tut.by>
2021-10-30 10:26:40 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,99 +1,97 @@
-var elById = document.getElementById('header'); // #header
-var elByClassName = document.getElementsByClassName('header'); // .header
-var elementsByClass = document.getElementsByClassName('some-class');
-var elByTag = document.getElementsByTagName('h1'); // h1
+var display = document.querySelector('.calculator__display');
+var numPad = document.querySelector('.calculator__numbers');
+var clear = document.querySelector('.calculator__numbers .clear');
+var actions = document.querySelector('.calculator__actions');
 
+var cache = [];
 
-var elQuerySelector = document.querySelector('.some-class');
-var elQuerySelectorAll = document.querySelectorAll('.some-class');
+function handleNumberClick(event) {
+    var target = event.target;
 
-console.log(elById);
-console.log(elByClassName);
-console.log(elementsByClass);
-console.log(elByTag);
-console.log(elQuerySelector);
-console.log(elQuerySelectorAll);
+    if (target.classList.contains('number')) {
+        var number = target.dataset.value;
 
+        if (number === '0' && !display.value) {
+            return;
+        }
 
-// elById.style.backgroundColor = "red";
-elById.style.fontSize = '40px';
-elById.classList.add('temporary');
-elById.classList.remove('temporary');
-elById.classList.toggle('temporary');
-
-console.log(elById.classList.contains('temporary'));
-
-function toggleBackground(event) {
-    event.target.classList.toggle('header');
-}
-
-function doSmth() {
-    console.log('smth');
-}
-
-// elById.onclick = toggleBackground;
-
-elById.addEventListener('click', toggleBackground);
-elById.addEventListener('click', doSmth);
-
-
-var newEl = document.createElement('div');
-
-newEl.innerHTML = `
-<div>
-    <div>
-        <h6>Deep</h6>
-    </div>
-</div>
-`;
-// newEl.innerText = '<span> a new element</span>';
-newEl.style.color = 'red';
-newEl.classList.add('new-span');
-
-var newSpan = document.createElement('span');
-newSpan.innerText = 'new span';
-
-
-elById.appendChild(newEl);
-elById.insertBefore(newSpan, newEl);
-
-var body = document.body;
-var remove = document.querySelector('div.some-class');
-
-// body.removeChild(remove);
-
-
-var fotoGalery = [
-    {
-        imgPath: './img/cat.jpg',
-        topContent: 'Top content',
-        bottomContent: 'BottomContent'
-    },
-    {
-        imgPath: './img/cat.jpg',
-        topContent: 'Top content 2',
-        bottomContent: 'BottomContent 2'
+        display.value = display.value + number;
     }
-]
+}
 
-var galeryItemMarkUp = function ({imgPath, topContent, bottomContent}) {
-    return `
-        <img src="${imgPath}" alt="">
-        <div class="description">
-            <div class="description__top">
-            ${topContent}
-            </div>
-            <div class="description__bottom">
-            ${bottomContent}
-            </div>
-        </div>
-    `;
+function handleClearClick (event) {
+    display.value = '';
+    cache = [];
+}
+
+function processAction (action) {
+    switch (action) {
+        case '=': {
+            cache.push(+display.value);
+            display.value = calculate(cache);
+            break;
+        }
+        default: {
+            cache.push(+display.value);
+            cache.push(action);
+            display.value = '';
+            display.focus();
+        }
+    }
+}
+
+function handleActionClick(event) {
+    var target = event.target;
+    if (target.classList.contains('action')) {
+        processAction(target.dataset.value);
+    }
+}
+
+function sum (a, b) {
+    return a + b;
+}
+
+function sub (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function devide (a , b) {
+    return a / b;
+}
+
+function customPow (a, b) {
+    return Math.pow(a, b);
+}
+
+var mathActionsMap = {
+    '+': sum,
+    '-': sub,
+    '*': multiply,
+    '/': devide,
+    'pow': customPow,
+}
+
+function calculate(arr) {
+    for(var i = 2; i < arr.length; i += 2) {
+        arr[i] = mathActionsMap[arr[i - 1]](arr[i - 2], arr[i]);
+    }
+
+    return arr[arr.length - 1];
+}
+
+function windowClickHandler (event) {
+    var supportedOperatons = ['+', '-', '/', '*', '='];
+    if (supportedOperatons.includes(event.key)) {
+        processAction(event.key);
+    }
 }
 
 
-for (var i = 0; i < fotoGalery.length; i++) {
-    var galeryItem = document.createElement('div');
-    galeryItem.classList.add('image');
-    galeryItem.innerHTML = galeryItemMarkUp(fotoGalery[i]);
-    document.body.appendChild(galeryItem);
-}
+numPad.addEventListener('click', handleNumberClick);
+clear.addEventListener('click', handleClearClick);
+actions.addEventListener('click', handleActionClick);
+window.addEventListener('keydown', windowClickHandler);
