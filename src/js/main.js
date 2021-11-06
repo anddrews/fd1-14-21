528f5c122fa3c54e3d7ec9cc567da91610913c44
andrei.kandratsenka <anddrews@tut.by>
2021-11-06 11:45:23 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-//here will by JavaScript code ...
+var items = document.querySelectorAll('.tree > .tree__item');
+
+items.forEach(function(item) {
+
+    var children = item.querySelectorAll('.tree-node__item + .tree-node__sub-items > .tree > .tree__item');
+
+    var el = item.querySelector('.tree-node__item');
+
+    if (children.length) {
+        el.dataset.childrenCount = children.length;
+        el.classList.add('has-children');
+    }
+})
