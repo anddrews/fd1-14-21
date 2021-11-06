f5a3febe47f874e9f73bacdd335edc61b7bf08d2
andrei.kandratsenka <anddrews@tut.by>
2021-11-06 10:05:29 +0300

    some changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,40 @@
 //here will by JavaScript code ...
+
+// var userInput = confirm('Tape smth');
+
+// if (userInput) {
+//     alert(userInput);
+// }
+
+var el = document.querySelector('#text-input');
+var frm = document.querySelector('form');
+var receiptes = document.querySelector('.receiptes');
+
+function handleInputChange (event) {
+    console.log(event.target.value);
+}
+
+function handleFormSubmit (event) {
+    event.preventDefault();
+
+    // for (var i = 0; i < event.target.length; i++) {
+    //     if (event.target[i].value) {
+    //         console.log(`${event.target[i].name} ${event.target[i].value}`);
+    //     }
+
+    // }
+    
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+        .then(resp => resp.json())
+        .then(data => {
+            receiptes.innerHTML = JSON.stringify(data);
+        })
+}
+
+frm.addEventListener('submit', handleFormSubmit);
+// el.addEventListener('blur', handleInputChange);
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+.then(resp => resp.json())
+.then(data => console.log(data))
+
